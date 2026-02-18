6243594240fb76d8d60ce25b69f926dc2f8f6763
Aman Singh <coolaman6299@gmail.com>
2026-02-18 19:26:03 +0530

    Added interceptor logic for the frontend

---
+++ frontend/src/lib/axios.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: `${process.env.BACKEND_URL}/api`,
+    withCredentials: true
+});
+
+api.interceptors.response.use((response) => response, async(error) => {
+    const originalRequest = error.config;
+    
+    if(error.response?.status == 401 && !originalRequest._retry){
+        originalRequest._retry = true;
+
+        try{
+            await api.post("/auth/refresh", {}, {
+                withCredentials: true
+            });
+
+            return api(originalRequest);
+        } catch(err) {
+            window.location.href = "/login";
+        }
+    }
+
+    return Promise.reject(error);
+});
+
+export default api;
